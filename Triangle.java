2c0b291de57f953668a81f12609dd805136c5b74
Meenakshi Manoj <73237796+Meenakshi-Manoj@users.noreply.github.com>
2021-01-12 10:45:08 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,23 @@
+3.Write a program to print the area and perimeter of a triangle having sides of 3, 4 and 5 units by creating
+a class named 'Triangle' without any parameter in its constructor.
+
+import java.util.*;
+public class Triangle
+{
+public Triangle()
+{
+ double perimeter,area;
+ int a=3, b=4, c=5;
+perimeter = a+b+c;
+double s=perimeter/2.0;
+area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+System.out.println("Area of triangle: "+area);
+System.out.println("Perimeter of Trinagle: "+perimeter);
+}
+ public static void main(String[]args){
+Triangle t = new Triangle();
+}
+}
+OUTPUT:
+Area of triangle: 6.0
+Perimeter of Triangle: 12.0
---
+++ ovrlod.java
@@ -0,0 +1,28 @@
+1.Write a Java class having overloaded methods to calculate area of rectangle
+and circle.
+
+class methods
+{
+ void area(double x)
+    {
+        double z = 3.14 * x * x;
+        System.out.println("the area of the circle is "+z);
+    }
+    void area(float x, float y)
+    {
+        System.out.println("the area of the rectangle is "+x*y);
+    }
+   
+}
+class ovrlod
+{
+     public static void main(String args[]) 
+	{
+	   methods ob = new methods();
+	   ob.area(8.5);
+	   ob.area(6,12);
+        }
+}
+OUTPUT:
+ the area of the circle is 226.865
+ the area of the rectangle is 72.0
